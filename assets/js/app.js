8ed9ac4bf1369309422138913134e682b50793e3
zilva149 <114057257+zilva149@users.noreply.github.com>
2023-03-25 10:44:28 +0200

    initial js logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
+class Calculator {
+  constructor(previousValueText, currentValueText) {
+    this.previousValueText = previousValueText;
+    this.currentValueText = currentValueText;
+  }
 
+  clear() {}
+
+  delete() {}
+
+  appendNumber() {}
+
+  chooseOperation() {}
+
+  compute() {}
+
+  getDisplayNumber() {}
+
+  updateDisplay() {}
+}
+
+const previousValueText = document.querySelector(".previous-value-text");
+const currentValueText = document.querySelector(".current-value-text");
+
+const btnClear = document.querySelector("button[data-clear]");
+const btnDelete = document.querySelector("button[data-delete]");
+const btnEquals = document.querySelector("button[data-equals]");
+const btnsNumber = document.querySelectorAll("button[data-number]");
+const btnsOperation = document.querySelectorAll("button[data-operation]");
+
+const calculator = new Calculator(previousValueText, currentValueText);
